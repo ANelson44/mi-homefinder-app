11f24f19d29143071b39b677b21bc4d3a3876a2f
Joshua Eagan <JoshuaPEagan@outlook.com>
2023-11-14 20:20:04 -0500

    fixed fetch request added script.js to results.html and cleared a card for injection from the api

--- script.js
+++ script.js
@@ -2,29 +2,12 @@
 // API URL: https://developer.nps.gov/api/v1/parks?stateCode=${stateCode}&api_key=${apiKey}
 
 
-import {
-    Ripple,
-    initTE,
-  } from "tw-elements";
+// import {
+//     Ripple,
+//     initTE,
+//   } from "tw-elements";
   
-  initTE({ Ripple });
+//   initTE({ Ripple });
 
-function findNationalParks() {
 
-  var stateCode = document.getElementById('state').value;
-  //var publicBathrooms = document.getElementById('publicBathrooms').checked;
-  //var rvAccess = document.getElementById('rvAccess').checked;
 
-  var apiKey = 'IIMr3waPpdOKznPvpyy5r3tNLTtNxOJ0KtZk3Xef';
-  var npsUrl = 'https://developer.nps.gov/api/v1/parks?stateCode=$' + stateCode + '&api_key=$' + apiKey;
-
-  fetch(npsUrl)
-    .then(response => response.json())
-    .then(data => {
-      console.log('API Data:', data);
-     displayNationalParks(data.data, publicBathrooms, rvAccess);
-
-  })
-  .catch(error => console.error('Error fetching data:', error));
-}
-findNationalParks();
