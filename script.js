e85e639e6d4740a97beb92124c647616dc3a1771
Joshua Eagan <JoshuaPEagan@outlook.com>
2023-11-15 17:59:15 -0500

    added functions to js

--- script.js
+++ script.js
@@ -8,6 +8,45 @@
 //   } from "tw-elements";
   
 //   initTE({ Ripple });
+function submitState() {
+  const stateCode = document.getElementById('state').value;
+  localStorage.setItem('selectedState', stateCode);
+  window.location.href = 'results.html';
+}
+
+function displayNationalParks() {
+  const stateCode = localStorage.getItem('selectedState');
+  const parksContainer = document.getElementById('parkCard')
+  //var publicBathrooms = document.getElementById('publicBathrooms').checked;
+  //var rvAccess = document.getElementById('rvAccess').checked;
+
+  var apiKey = 'IIMr3waPpdOKznPvpyy5r3tNLTtNxOJ0KtZk3Xef';
+  var npsUrl = `https://developer.nps.gov/api/v1/parks?stateCode=${stateCode}&api_key=${apiKey}`;
+
+  fetch(npsUrl)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(apple){
+      console.log('API Data:', apple);
+      const parks = apple.data
+      let cardHtml = '' 
+      parks.forEach(park => {
+        const parkName = park.fullName
+        // cardHtml += `<li>${parkName}</li>` 
+        cardHtml += `
+        <div class="border-solid border-2 border-red-600">
+          <div class="font-bold text-xl mb-2" id="parkName">Park Name: ${parkName}</div>
+          <div class="font-bold text-l mb-2">State: ${park.states}</div>
+        </div>
+        `
+      });
+      console.log('Park Name: ', cardHtml);
+      $('#parkList').html(cardHtml)
+    })
 
 
-
+}
+if (window.location.pathname.includes('results.html')) {
+  displayNationalParks();
+}
