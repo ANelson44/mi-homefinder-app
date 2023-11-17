50c577c2eb3e9d793eada6d308562213b9b8b6b7
zach <winchelzachary@gmail.com>
2023-11-16 20:28:43 -0500

    linked show map button to map page

--- map.js
+++
@@ -1,6 +0,0 @@
-// JavaScript for map
-
-function showMap(parkName, latitude, longitude) {
-    const mapPageUrl = `map.html?name=${encodeURIComponent(parkName)}&lat=${latitude}&lng=${longitude}`;
-    window.location.href = map.html;
-}
